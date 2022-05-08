e0af5c0569f633cb6b490626a79a426bdcc15178
paczek07 <jesmdz@gmail.com>
2022-05-08 22:28:50 +0300

    final of the day

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -5,15 +5,13 @@ const Projects = () => {
   return (
     <section id="projects" class=" px-16 mx-auto my-32">
 
-        <h2 class="text-4xl font-bold text-center my-10">
-          Projects?
+        <h2 class="text-4xl font-bold text-center mb-20 dark:text-white">
+          Projects
         </h2>
-        <p class="my-5">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquid optio officia perspiciatis ex tenetur maiores quos. Earum modi numquam itaque hic. Fugiat, iure ex? Quibusdam nesciunt ipsum ullam incidunt consequatur tempora nulla dolor voluptatibus ab. Veniam minima animi totam at, amet facere enim tenetur praesentium voluptates, quae placeat sint quasi!</p>
         <div class="grid gap-6 md:grid-cols-2 xl:grid-cols-3 mt-10">
         <Card projectName="Bug Tracker"/>
         <Card projectName="Habit Tracker"/>
         <Card projectName="Witcher API"/>
-        <Card projectName="Phone App"/>
         </div>
     </section>
 
